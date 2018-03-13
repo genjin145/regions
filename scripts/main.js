577414ed89fbfdadec18d9d310d852643a6378d8
Anonim <genjin145@gmail.com>
2018-03-14 04:12:30 +0500

    add section

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,41 @@
 var main = document.getElementsByTagName("main")[0],
-    p = document.getElementsByTagName("p");
+    p = document.getElementsByTagName("p"),
+    td = info.getElementsByTagName("td"),
 
-for (var i = 0; i < db.length; i++) {
-    main.appendChild(document.createElement("p"));
+    date = new Date();
+
+
+show_all.onclick = function() {
+    result.classList.toggle("hide");
+    if (result.getAttribute("class") == "hide") {
+        this.textContent = "Скрыть все регионы";
+        result.style.display ="block";
+        for (var i = 0; i < db.length; i++) {
+            result.appendChild(document.createElement("p"));
+            p[i].textContent = db[i].id + " - " + db[i].name;
+        }
+    } else {
+        this.textContent = "Показать все регионы";
+        result.style.display ="none";
+    }
+}
+
+function convert_hour(gmt) {
+    if (date.getUTCHours() + gmt > 24) {
+        return date.getUTCHours() + gmt - 24;
+    } else {
+       return date.getUTCHours() + gmt;
+    } 
+}
+
+search.oninput = function() {
+    date = new Date();
+    for (var i = 0; i < db.length; i++) {
+        if (this.value == db[i].id) {
+            td[0].textContent = db[i].id;
+            td[1].textContent = db[i].name;
+            td[2].textContent = convert_hour(db[i].time) + ":" + date.getUTCMinutes() + " (GMT +" + db[i].time + ")";
+        }
+    }
     
-    p[i].textContent = db[i].name;
 }
