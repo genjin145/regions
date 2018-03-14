0704f253e685c4a93ce7978254ad27313f3d6a5a
Anonim <genjin145@gmail.com>
2018-03-14 05:23:52 +0500

    search for region codes

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,21 @@ var main = document.getElementsByTagName("main")[0],
     p = document.getElementsByTagName("p"),
     td = info.getElementsByTagName("td"),
 
-    date = new Date();
+    date = new Date(),
+    search_code;
 
 
 show_all.onclick = function() {
-    result.classList.toggle("hide");
-    if (result.getAttribute("class") == "hide") {
+    result.classList.toggle("show");
+    if (result.getAttribute("class") == "show") {
         this.textContent = "Скрыть все регионы";
         result.style.display ="block";
         for (var i = 0; i < db.length; i++) {
             result.appendChild(document.createElement("p"));
             p[i].textContent = db[i].id + " - " + db[i].name;
+            if (search_code == db[i].id) {
+                p[i].classList.add("mark");
+            }
         }
     } else {
         this.textContent = "Показать все регионы";
@@ -29,13 +33,34 @@ function convert_hour(gmt) {
 }
 
 search.oninput = function() {
-    date = new Date();
-    for (var i = 0; i < db.length; i++) {
-        if (this.value == db[i].id) {
-            td[0].textContent = db[i].id;
-            td[1].textContent = db[i].name;
-            td[2].textContent = convert_hour(db[i].time) + ":" + date.getUTCMinutes() + " (GMT +" + db[i].time + ")";
-        }
+    if (this.value.length < 2) {
+        search_code = this.value;
+    } else {
+        search_code = this.value.slice(0, 2);
     }
     
+    if (Number(search_code)) {
+        date = new Date();
+        for (var i = 0; i < db.length; i++) {
+            if (result.getAttribute("class") == "show" && search_code == db[i].id) {
+                p[i].classList.add("mark");
+            } else if (result.getAttribute("class") == "show") {
+                p[i].classList.remove("mark");
+            }
+
+            if (search_code == db[i].id) {
+                td[0].textContent = db[i].id;
+                td[1].textContent = db[i].name;
+                td[2].textContent = convert_hour(db[i].time) + ":" + date.getUTCMinutes() + " (GMT +" + db[i].time + ")";
+            }
+        }
+    }
+    if (search_code == "") {
+        td[0].textContent = " ";
+        td[1].textContent = " ";
+        td[2].textContent = " ";
+//        for (var i = 0; i < db.length; i++) {
+//            p[i].classList.remove("mark");
+//        }
+    }
 }
